8eecfb1e692518a60b7a3f2c38fccb019dcc396f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-01 19:41:08 +0200

    fix

--- src/main/java/fi/johvu/motimusic/Horn.java
+++ src/main/java/fi/johvu/motimusic/Horn.java
@@ -2,6 +2,7 @@ package fi.johvu.motimusic;
 
 import com.xxmicloxx.NoteBlockAPI.NoteBlockAPI;
 import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.songplayer.PositionSongPlayer;
 import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer;
 import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
 import org.bukkit.command.Command;
@@ -21,32 +22,37 @@ public class Horn implements CommandExecutor {
 	}
 
 
-
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player player = (Player) sender;
 
-		if(args.length == 1){
-			if(args[0] == null){
+		if (args.length == 1) {
+			if (args[0] == null) {
 				return false;
 			}
-			if(args[0].equals("menu")){
-					main.openMusicInv("horn", player);
+			if (args[0].equals("menu")) {
+				main.openMusicInv("horn", player);
 			}
-			if(args[0].equals("jones")){
-				com.xxmicloxx.NoteBlockAPI.model.Song song = NBSDecoder.parse(new File(main.getDataFolder(), "4.nbs"));
-				RadioSongPlayer rsp = new RadioSongPlayer(song);;
+
+			if (args[0].equals("jones")) {
+				com.xxmicloxx.NoteBlockAPI.model.Song song = NBSDecoder.parse(new File(main.getDataFolder(), "/songs/horn/4.nbs"));
 				player.sendMessage("Soitetaan");
-				rsp.addPlayer(player);
-				rsp.setPlaying(true);
+				PositionSongPlayer psp = new PositionSongPlayer(song);
+				psp.setTargetLocation(player.getLocation());
+				psp.setDistance(16); // Default: 16
+				psp.addPlayer(player);
+				psp.setPlaying(true);
+				do {
+					psp.setTargetLocation(player.getLocation());
+
+				} while (psp.isPlaying());
 			}
-			if(args[0].equals("stop")){
+			if (args[0].equals("stop")) {
 				NoteBlockAPI.stopPlaying(player.getUniqueId());
 			}
 
 			return false;
+
 		}
 		return false;
-
 	}
-
 }
